e75e5df6ce60fcacf033454105f3050a133d42cb
Matt Ondo <yours.truly@mattondo.com>
2023-12-02 12:07:44 -0700

    remove acf dependencies for cleaner install

--- includes/acf/blocks/button/block.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  const links = document.querySelectorAll('.editor-styles-wrapper .wp-block-acf-button a');
-  links.forEach(link => {
-    link.addEventListener('click', e => {
-      console.log(link);
-      e.preventDefault();
-    });
-  });
-})()
--- includes/acf/readme.md
+++
@@ -1,3 +0,0 @@
-# ACF 
-
-This directory is an example for managing ACF custom fields & ACF custom blocks via PHP. Requires ACF to be installed.
